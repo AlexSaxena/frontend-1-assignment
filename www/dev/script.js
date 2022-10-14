b27b61a41dc3f245d61f7d7e9070ef29d3cce54b
AlexSaxena <saxenaalex@gmail.com>
2022-10-14 10:09:56 +0200

    Adds basic sort functionality WIP

--- www/dev/script.js
+++ www/dev/script.js
@@ -7,13 +7,67 @@ function getContentFromApi(filename, selector) {
       hljs.highlightAll(); // hljs (highlight.js) ger syntax-highlight i <code>-taggar.
     });
 }
-getContentFromApi('ovningar1-10.md', '[data-source="content"]')
+//getContentFromApi("ovningar1-10.md", '[data-source="content"]');
 
 function getUsers() {
   fetch("http://localhost:4000/api/users/")
     .then((response) => (response = response.json()))
     .then((users) => {
-      console.table(users);
+      sortByColour(users);
+
+      // console.table(users);
+      // console.log(typeof users);
+
+      let newSection = document.querySelector(".newSection");
+
+      users.forEach((user) => {
+        for (let key in user) {
+          let element = document.querySelector(`[data-source=${key}]`);
+          if (element) {
+            element.innerText = user[key];
+          }
+        }
+        for (const [key, value] of Object.entries(user)) {
+          //console.log(`${key}: ${value}`);
+          let newP = document.createElement("p");
+          newP.innerText = `${key}: ${value}!`;
+          newSection.append(newP);
+        }
+      });
     });
 }
+
+function sortByColour(users) {
+  console.log("Inuti sortByColour");
+  let userArrayRed = ["Röd"];
+  let userArrayYellow = ["Gul"];
+  let userArrayBlue = ["Blå"];
+  let userArrayGreen = ["Grön"];
+  let userArrayRest = ["rest"];
+
+  console.log(users);
+  for (const key in users) {
+    if (Object.hasOwnProperty.call(users, key)) {
+      const element = users[key];
+      // console.log("This is an ->" + typeof element);
+      // console.log("userColorType ->" + element.personalityType);
+      // console.log("TypeOF color ->" + typeof element.personalityType);
+      // console.table(element);
+      if (element.personalityType === "gul") {
+        userArrayYellow.push(element);
+      } else if (element.personalityType === "blå") {
+        userArrayBlue.push(element);
+      } else if (element.personalityType === "röd") {
+        userArrayRed.push(element);
+      } else if (element.personalityType === "grön") {
+        userArrayGreen.push(element);
+      } else {
+        userArrayRest.push(element);
+      }
+    }
+  }
+  console.log(userArrayBlue);
+  console.log(userArrayYellow);
+}
+
 getUsers();
